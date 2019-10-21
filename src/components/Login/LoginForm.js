35f04733f45deafa4c64357eee1970029d2c2cca
anderara <andc91@gmail.com>
2019-10-21 15:57:59 +0200

    Add Login container

---
+++ src/components/Login/LoginForm.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class LoginForm extends Component {
+	render() {
+		const { onChange, onSubmit } = this.props
+		const { email, password } = this.props.values
+		return (<div>
+				<form onSubmit={onSubmit}>
+					<input type="text" 
+						name="email" 
+						placeholder="E-mail" 
+						value={email} 
+						onChange={onChange} 
+					/>
+					<input type="password" 
+						name="password" 
+						placeholder="password" 
+						value={password} 
+						onChange={onChange}
+						/>
+					
+					<button type="submit" className="btn btn-primary w-100">Login</button>
+				</form>
+				<p>Don't have an account yet? <Link to="/signup">Sign up</Link></p>
+                </div>
+		)
+	}
+}
+
+export default LoginForm;
