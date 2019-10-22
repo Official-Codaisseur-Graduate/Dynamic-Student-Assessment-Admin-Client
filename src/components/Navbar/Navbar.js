3dbac0c09acc0be8531881a3ceff4fd666756d0d
debesteste <gersonlynch@gmail.com>
2019-10-22 14:35:25 +0200

    Added some styling on the subnav and nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,28 +2,35 @@ import React, { Component} from 'react'
 import {Link} from 'react-router-dom'
 import SubNavigation from '../SubNavigation/SubNavigation'
 
+
+
 export default class Navbar extends Component {
     render() {
+      let s1 = {verticalAlign: 'center'};
+      let s2 = {textAlign: 'center'};
         return (
 
           <div>
-            <nav>
-
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/signup">Sign Up</Link>
-                </li>
-                <li>
-                  <Link to="/login">Log In</Link>
-                </li>
-                <li>
-                  <Link to='/logout' classname="logout">Log Out</Link></li>
-              </ul>
-            <SubNavigation />
-            </nav>
+          
+       <table width="30%">
+         <tbody>
+           <tr style={s1}>
+             <td className="mui--appbar-height" style={s2}>
+               <Link to="/" className="mui--text-title">Home</Link>
+              </td>
+             <td className="mui--appbar-height" style={s2}>
+             <Link to="/signup" className="mui--text-title">Sign Up</Link>
+             </td>
+             <td className="mui--appbar-height" style={s2}>
+             <Link to="/login" className="mui--text-title">Log In</Link>
+             </td>
+             <td className="mui--appbar-height" style={s2}>
+             <Link to='/logout' className="logout mui--text-title">Log Out</Link>
+             </td>
+           </tr>
+         </tbody>
+       </table>
+       <SubNavigation />
           </div>
         )
     }
