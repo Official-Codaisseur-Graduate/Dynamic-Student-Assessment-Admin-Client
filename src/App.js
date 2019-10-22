06a37290b2ed374b4ae250e94b22a77f5cdeabb6
debesteste <gersonlynch@gmail.com>
2019-10-22 14:54:50 +0200

    Styling on add question form

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import LoginContainer from './components/Login/LoginContainer'
 import SignupContainer from './components/Signup/SignupContainer';
 import AddQuestionContainer from './components/AddQuestion/AddQuestionContainer'
 import LogoutContainer from './components/Logout/LogoutContainer'
-import AddQuestionForm from './components/AddQuestion/AddQuestionForm';
+
 
 
 
