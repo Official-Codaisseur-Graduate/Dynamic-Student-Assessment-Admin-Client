fcdc0e1fc5ca4375e6bdd7047c1cecfd807dc08c
anderara <andc91@gmail.com>
2019-10-22 11:25:58 +0200

    Add LogoutContainer in both App.js & Navbar.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import './App.css';
 //Components
 import NavbarContainer from './components/Navbar/NavbarContainer'
 import LoginContainer from './components/Login/LoginContainer'
-import SignupContainer from './components/Signup/SignupContainer';
+import SignupContainer from './components/Signup/SignupContainer'
+import LogoutContainer from './components/Logout/LogoutContainer'
 
 
 function App() {
