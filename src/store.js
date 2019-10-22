9ce0d1fbd6e3a9cbc14b4590cc031edf279d0ea6
anderara <andc91@gmail.com>
2019-10-22 11:52:56 +0200

    Edit store.js and index.js

--- src/store.js
+++ src/store.js
@@ -1,11 +1,16 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+// import reducer from './reducers'
+import ReduxThunk from 'redux-thunk'
+// import { loadState, saveState } from './localStorage'
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const enhancer = composeEnhancers(
   applyMiddleware(ReduxThunk)
 )
 
-const persistedState = loadState()
-const store = createStore(reducer, persistedState, enhancer)
+// const persistedState = loadState()
+const store = createStore( enhancer)
 
 //BELOW IS THE FUNCTION FOR LOCAL STORAGE JWT
 
