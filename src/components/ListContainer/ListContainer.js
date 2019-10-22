fb8c345dece3a9f509a02ed44984852d2243bdf4
debesteste <gersonlynch@gmail.com>
2019-10-22 11:53:08 +0200

    Added question list component

---
+++ src/components/ListContainer/ListContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import List from './List'
+
+export default class ListContainer extends Component {
+    render() {
+        return (
+            <div>
+                <List />
+            </div>
+        )
+    }
+}
