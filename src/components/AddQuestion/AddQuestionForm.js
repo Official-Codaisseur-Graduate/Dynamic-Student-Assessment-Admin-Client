5cc14e743bc77958c9f2170703e58baee6909f4b
debesteste <gersonlynch@gmail.com>
2019-10-23 11:03:42 +0200

    Matched styling with sign up form

--- src/components/AddQuestion/AddQuestionForm.js
+++ src/components/AddQuestion/AddQuestionForm.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
 
@@ -15,7 +12,7 @@ export default class AddQuestionForm extends Component {
         const { onChange, onSubmit } = this.props;
 		const { question, rightAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3 } = this.props.values;
         return (
-            <div className="spacer-lg">
+            <div>
                  
                 <Container>
                  <Card className="card-50 component-center">
