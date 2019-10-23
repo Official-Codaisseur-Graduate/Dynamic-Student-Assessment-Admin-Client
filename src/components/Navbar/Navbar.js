3d1ea23dabc1c00ef760352468e94f844a145a51
debesteste <gersonlynch@gmail.com>
2019-10-23 15:23:20 +0200

    Changed logo in navbar and home route

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import SubNavigation from '../SubNavigation/SubNavigation'
 import Link from '@material-ui/core/Link';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import picture from '../../constants/icon.png'
 
 
 
@@ -14,8 +15,9 @@ export default class Navbar extends Component {
           <div>
           <AppBar variant="fixed" className="bg-white" position="static">
             <Toolbar>  
-                <Link href="/home" >
-                <img className="logo pt-1" src='https://i1.wp.com/thenextwomen.com/summit/wp-content/uploads/2017/03/codaisseur-logo-long-2e9b7b7b839a33128016eb77b49ff2bb6397b6b3552523ee09273d6a5b427861.png?resize=1200%2C279&ssl=1' alt="codaisseur logo" width="260"/>
+                <Link href="/" >
+                <img src="https://vectr.com/tmp/a5Cpw0Nj34/cl8ePfAwW.png?width=62.19&height=91.77&select=aFphC0vP&source=selection" width="50px"></img>
+                {/* <img className="logo pt-1" src='https://i1.wp.com/thenextwomen.com/summit/wp-content/uploads/2017/03/codaisseur-logo-long-2e9b7b7b839a33128016eb77b49ff2bb6397b6b3552523ee09273d6a5b427861.png?resize=1200%2C279&ssl=1' alt="codaisseur logo" width="260"/> */}
               </Link>
               <Link href="/signup" color="inherit" >
               <p className="text-dark text-bold-spaced nav-link pt-1">Sign Up</p>
