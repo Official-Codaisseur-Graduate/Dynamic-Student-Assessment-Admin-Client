c805fc34cc852e6fd152886d60c3e704d85fb559
debesteste <gersonlynch@gmail.com>
2019-10-24 15:47:24 +0200

    Loading questions from database with associated answers and category

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux'
 import user from './user'
+import questions from'./questions'
+import answers from './answers'
 
 export default combineReducers({
-    user
-
+    user,
+    questions,
+    answers
 })
