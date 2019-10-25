6653fb169269a4f0080b55db4727e3f1356eafd6
debesteste <gersonlynch@gmail.com>
2019-10-25 16:20:18 +0200

    Fetching data from databse and mapping students/teachers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import user from './user'
 import addedquestion from './addedquestion'
 import questions from'./questions'
+import allUsers from './allUsers'
 
 export default combineReducers({
     user,
     questions,
     addedquestion,
+    allUsers
 })
