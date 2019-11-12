9c4feb32b2de790ee667f15c7a20d2f311ba38b8
stijnblokker <stijnblokker@gmail.com>
2019-11-12 14:25:23 +0100

    inplemented sign up and log out features

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,12 +3,14 @@ import React, { Component } from "react"
 import SubNavigation from "../SubNavigation/SubNavigation"
 // import Link from '@material-ui/core/Link';
 import { Link } from "react-router-dom"
+import { connect } from "react-redux";
 
 
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import { removeUnderline } from "../../constants/style"
-export default class Navbar extends Component {
+
+class Navbar extends Component {
 	render() {
 		return (
 			<div>
@@ -19,29 +21,32 @@ export default class Navbar extends Component {
 							{/* <img className="logo pt-1" src='https://i1.wp.com/thenextwomen.com/summit/wp-content/uploads/2017/03/codaisseur-logo-long-2e9b7b7b839a33128016eb77b49ff2bb6397b6b3552523ee09273d6a5b427861.png?resize=1200%2C279&ssl=1' alt="codaisseur logo" width="260"/> */}
 						</Link>
 
-						<Link to="/signup" color="inherit" style={removeUnderline}>
+						{!this.props.loggedIn ? (
 							<p className="text-dark text-bold-spaced nav-link pt-1">
-								Sign Up
+							<Link to="/signup" color="inherit" style={removeUnderline}> Sign Up </Link> 
+							| 
+							<Link to="/login" style={removeUnderline}> Sign In </Link>
 							</p>
-						</Link>
+						 ) : (
+								<Link to="/logout" style={removeUnderline}>
+									<p className="text-dark text-bold-spaced nav-link pt-1">Log Out</p>
+								</Link>
+							)}
 
-						<Link to="/login" style={removeUnderline}>
-							<p className="text-dark text-bold-spaced nav-link pt-1">
-								Sign In
-							</p>
-						</Link>
-						<Link to="/logout" style={removeUnderline}>
-							<p className="text-dark text-bold-spaced nav-link pt-1">
-								Log Out
-							</p>
-						</Link>
+
 					</Toolbar>
 				</AppBar>
-
-
-				<SubNavigation />
+						 {this.props.loggedIn ? (<SubNavigation show={true} />) : ( <SubNavigation show={false} />) }	
 			</div>
 		)
 	}
 
 }
+
+const mapStateToProps = state => {
+	return {
+		loggedIn: state.user.jwt !== null
+	};
+};
+
+export default connect(mapStateToProps)(Navbar);
--- src/components/SubNavigation/SubNavigation.js
+++ src/components/SubNavigation/SubNavigation.js
@@ -6,31 +6,31 @@ import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import { removeUnderline } from "../../constants/style"
 
-export default class SubNavigation extends Component {
-	render() {
-		return (
-			<div>
-				<AppBar className="bg-dark" position="static">
-					<Toolbar>
-						<Link to="/add-question" style={removeUnderline}>
-							<small className="text-white text-bold-spaced fs-14 nav-link">
-								Add Questions
+export default function SubNavigation ( {show} ) {
+	return (
+		<div>
+			<AppBar className="bg-dark" position="static">
+				{show && 
+				<Toolbar>
+				<Link to="/add-question" style={removeUnderline}>
+						<small className="text-white text-bold-spaced fs-14 nav-link">
+							Add Questions
 							</small>
-						</Link>
-						<Link to="/questions" style={removeUnderline}>
-							<small className="text-white text-bold-spaced fs-14 nav-link">
-								Questions
+					</Link>
+					<Link to="/questions" style={removeUnderline}>
+						<small className="text-white text-bold-spaced fs-14 nav-link">
+							Questions
 							</small>
-						</Link>
-						<Link to="/students" style={removeUnderline}>
-							<small className="text-white text-bold-spaced fs-14 nav-link">
-								Students
+					</Link>
+					<Link to="/students" style={removeUnderline}>
+						<small className="text-white text-bold-spaced fs-14 nav-link">
+							Students
 							</small>
-						</Link>
-					</Toolbar>
-				</AppBar>
+					</Link>
+				</Toolbar>
+				}
+			</AppBar>
 
-			</div>
-		)
-	}
+		</div>
+	)
 }
