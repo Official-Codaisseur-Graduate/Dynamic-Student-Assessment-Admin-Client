ab7db1069647b0b76be47b27da118cbb83686dcc
qiyu1987 <qiyu1987@gmail.com>
2019-11-15 15:27:16 +0100

    fix comma missing

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,20 +2,15 @@ import { combineReducers } from "redux"
 import user from "./user"
 import newQuestion from "./newQuestion"
 import questions from "./questions"
-import students from './students'
-
-import generatecode from './generatecode'
-
-import categories from './categories'
+import students from "./students"
+import generatecode from "./generatecode"
+import categories from "./categories"
 
 export default combineReducers({
 	user,
 	questions,
 	students,
 	newQuestion,
-
-	generatecode
-
+	generatecode,
 	categories
 })
-
